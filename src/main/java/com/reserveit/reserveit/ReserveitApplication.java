69a73ca70876d445bd7a3df0d69bd491c8f8ac62
dianaschneider <diana.teodora.schneider@gmail.com>
2023-05-30 00:13:21 +0300

    Initial commit for backend

---
+++ src/main/java/com/reserveit/reserveit/ReserveitApplication.java
@@ -0,0 +1,13 @@
+package com.reserveit.reserveit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReserveitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reserveit/reserveit/ReserveitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reserveit.reserveit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
